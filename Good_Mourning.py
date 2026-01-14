36e9165297b345d08893497843697524bf655ed9
jayknowso <worklife0524@gmail.com>
2026-01-14 01:47:54 -0500

    added notes for combined basics in good mourning file

--- Good_Mourning.py
+++ Good_Mourning.py
@@ -1,15 +1,18 @@
+# Notes: Practicing user input
+# Using if/ elif/ else for decision making
+# Variables store user responses
+# f-strings format output messages
 import datetime
 # get current hour
 current_hour = datetime.datetime.now().hour
 
-#decide time of day
+# decide time of day
 if current_hour < 12:
-    time_of_day = "mourning"
+    time_of_day = "Mourning"
 elif current_hour < 18:
-    time_of_day = "afternoon"
+    time_of_day = "Afternoon"
 else: 
-    time_of_day = "evening"
-
+    time_of_day = "Evening"
  # ask user for name
-name = input("enter your name:")    # the person we want to greet
-print(f"good {time_of_day}, {name}!")
+name = input("enter your name:") # the person we want to greet
+print(f"Good {time_of_day}, {name}!")
